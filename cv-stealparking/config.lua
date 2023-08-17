66e71567dad4ae52c04a1ac79759eed617d70b62
NaorShabtai <111662813+NaorShabtai@users.noreply.github.com>
2023-08-17 14:32:31 +0300

    Update config.lua
    Added explanations

--- cv-stealparking/config.lua
+++ cv-stealparking/config.lua
@@ -1,10 +1,10 @@
 Config = {}
 
-Config.MinMoney = 100
+Config.MinMoney = 100 -- minimum money reward
 
-Config.MaxMoney = 500
+Config.MaxMoney = 500 -- maximum money reward
 
-Config.Cooldown = 2000 -- in milisec
+Config.Cooldown = 2000 -- in milliseconds
 
-Config.dispatch = true
+Config.dispatch = true -- if you want dispatch call set it true but if you don't want set it false
 
